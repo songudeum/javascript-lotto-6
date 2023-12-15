d743535e3bf285873e26103c148da45a2ca483f2
songudeum <songudeum@gmail.com>
2023-12-15 21:19:59 +0900

    feat: 에러 메시지 대본 추가

---
+++ src/utils/script.js
@@ -0,0 +1,17 @@
+const INSUFFICIENT_LOTTO_NUMBERS_ERROR = "[ERROR] 로또 번호는 6개여야 합니다.";
+
+const INVALID_LOTTO_NUMBERS_ERROR =
+    "[ERROR] 로또 번호는 중복되지 않은 1~45 사이의 숫자이어야 합니다.";
+
+const INVALID_BONUS_NUMBER_ERROR =
+    "[ERROR] 보너스 번호는 1~45사이의 하나의 숫자이어야 합니다.";
+
+const DUPLICATE_BONUS_NUMBER_ERROR =
+    "[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.";
+
+export {
+    INSUFFICIENT_LOTTO_NUMBERS_ERROR,
+    INVALID_LOTTO_NUMBERS_ERROR,
+    INVALID_BONUS_NUMBER_ERROR,
+    DUPLICATE_BONUS_NUMBER_ERROR,
+};
